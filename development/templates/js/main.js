5f59d2f1a34030c6f1a6adc38dd2c5313c8de9b2
GeRi <2rcsanyi.g@gmail.com>
2021-11-18 22:25:28 +0100

    UX improvements added

--- development/templates/js/main.js
+++ development/templates/js/main.js
@@ -2,7 +2,6 @@
 let sheet = new GoogleSheet(GOOGLE_SHEET_ID, GOOGLE_API_KEY);
 
 function draw_panels(panels){
-    console.log('DRAW PANELS');
     let template = `<div class="flip _EXTRA_CSS_" id="_NAME_">_NAME_</div>
     <div class="panel" style="overflow: hidden;">
         <div class="products">
@@ -26,6 +25,7 @@ function draw_panels(panels){
 }
 
 function google_sheet_callback(data){
+    
     data = GoogleSheet.parse_raw_data(data);
     let products_div = document.getElementById(data.sheet_name).nextElementSibling.firstElementChild;
     let template = `<div class="products-box">
@@ -38,8 +38,8 @@ function google_sheet_callback(data){
 </div>
     `
     for (product of data.values){
-        //let html = template.replaceAll('_IMAGE_URL_', product.img);
-        let html = template.replaceAll('_IMAGE_URL_', 'static/szárazáru/lencse.jpg');
+        let html = template.replaceAll('_IMAGE_URL_', product.img);
+        //let html = template.replaceAll('_IMAGE_URL_', 'static/szárazáru/lencse.jpg'); // TESZT
         html = html.replaceAll('_PRODUCT_NAME_', product.name);
         html = html.replaceAll('_PRODUCT_PRICE_', product.price);
         html = html.replaceAll('_PRODUCT_UNIT_', product.unit);
@@ -47,9 +47,13 @@ function google_sheet_callback(data){
         products_div.insertAdjacentHTML('beforeend', html);
     }
 
-    let back_button = `<a class="close_panel" href="#/">Vissza</a>`;
-    products_div.insertAdjacentHTML('beforeend', back_button);
+    let close_button_html = `<a class="close_panel" href="#/">Vissza</a>`;
+    products_div.insertAdjacentHTML('beforeend', close_button_html);
 
+    let close_button = products_div.children[products_div.children.length - 1];
+    close_button.addEventListener('click', function (e) {
+        slideToggle(close_button.parentElement.parentElement, 500)
+    });
 }
 
 function setup_panel_event_listeners(){
@@ -62,33 +66,30 @@ function setup_panel_event_listeners(){
 
             // Check if data is already loaded
             if (panel.firstElementChild.children.length <= 1){
-                console.log(panel.firstElementChild.children.length);
                 let category = this.id;
                 sheet.get_data_from_sheet(category, 'google_sheet_callback');
-                console.log(category);
-                // TODO slideToggle only when data is loaded
             }
-            
-            slideToggle(panel, 500);
+
             all_panels = document.querySelectorAll('.panel');
             all_panels.forEach(function(el) {
                 if (el.style.display == 'block' && el !== panel) slideUp(el, 200);
             });
+
+            // With this magic loop we're waiting until
+            // there are some elements in the panel
+            var interval_loop = setInterval(function(){
+                if(panel.firstElementChild.children.length > 1) {
+                    slideToggle(panel, 500);
+                    clearInterval(interval_loop);
+                }
+            }, 300);
+            
         });
     }
 
-    const close_panel_buttons = document.querySelectorAll('.close_panel');
 
-    for (let i = 0; i < close_panel_buttons.length; i++) {
-        close_panel_buttons[i].addEventListener('click', function (e) {
-            slideToggle(e.target.parentElement.parentElement, 500)
-        });
-    }
 }
 
-
-
-console.log('LOAD');
 draw_panels(CATEGORIES);
 setup_panel_event_listeners();
 
